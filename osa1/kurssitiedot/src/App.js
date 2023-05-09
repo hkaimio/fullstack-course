482d9dcf969ee32275685621b40b30fea553f7d6
Harri Kaimio <harri@kaimio.fi>
2023-05-09 21:06:55 +0300

    Ex 1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => {
     return (
@@ -27,26 +29,29 @@ const App = () => {
 
     )
   }
-  const Content = () => {
+  const Content = (props) => {
     return (
       <>
-      <Part part={part1}/>
-      <Part part={part2}/>
-      <Part part={part3}/>
+      {props.parts.map(p => <Part part={p}/>)}
       </>
     )
   }
 
   const Total = (props) => {
+
+    let tot = 0;
+    for (const p of parts) {
+      tot += p.exercises;
+    }
     return (
-      <p>Number of exercises {props.count}</p>
+      <p>Number of exercises {tot}</p>
     )
   }
   return (
     <div>
       <Header course={course}/>
-      <Content/>
-      <Total count={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
       
     </div>
   )
