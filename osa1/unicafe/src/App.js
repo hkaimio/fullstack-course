7e860bd72a830ce50110d175d8f64d3f9a97410c
Harri Kaimio <harri@kaimio.fi>
2023-05-10 21:10:18 +0300

    Ex 1.8 & 1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -2,6 +2,29 @@ import { useState } from 'react'
 
 const Button = ({text, clickHandler}) => <button onClick={clickHandler}>{text}</button>
 
+const Statistics = ({good, bad, neutral}) => {
+  const total = good + bad + neutral
+
+  if (total > 0) {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>Total {good + bad + neutral}</p>
+      <p>Average {(good - bad) / total}</p>    
+    </>
+  )}
+  else {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+  
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -14,12 +37,7 @@ const App = () => {
       <Button  text="Good" clickHandler={() => setGood(good+1)}/>
       <Button  text="Neutral" clickHandler={() => setNeutral(neutral+1)}/>
       <Button  text="Bad" clickHandler={() => setBad(bad+1)}/>
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>Total {good + bad + neutral}</p>
-      <p>Average {(good - bad) / (good + bad + neutral)}</p>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
