f58ae29fe904baafbc47dd78f6c625d2d60ae7b1
Harri Kaimio <harri@kaimio.fi>
2023-05-12 19:46:51 +0300

    Ex 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,3 +1,5 @@
+import Course from './Course.js'
+
 const App = () => {
   const courses = [
     {
@@ -44,49 +46,6 @@ const App = () => {
     }
   ]
 
-  const Header = (props) => {
-    return (
-      <h1>{props.course}</h1>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-
-    )
-  }
-  const Content = (props) => {
-    return (
-      <>
-      {props.parts.map(p => <Part key={p.id} part={p}/>)}
-      </>
-    )
-  }
-
-  const Total = ({parts}) => {
-
-    const tot = parts.reduce((exCount, part) => {
-      return exCount + part.exercises
-    }, 0 )
-
-    return (
-      <p><strong>Number of exercises {tot}</strong></p>
-    )
-  }
-
-  const Course = ({ course }) => {
-    return (
-      <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-
-      </div>
-    )
-  }
 
   return (
     <div>
