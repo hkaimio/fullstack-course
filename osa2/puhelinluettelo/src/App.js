37c087f7d581611b551fa9464ee7eb4c28fd3c43
Harri Kaimio <harri@kaimio.fi>
2023-05-12 22:48:13 +0300

    Ex 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const PhonebookEntry = ({person}) => {
   return (
-    <p>{person.name}: {person.phone}</p>
+    <p>{person.name}: {person.number}</p>
   )
 }
 
@@ -45,12 +46,18 @@ const PersonList = ({persons, filterStr}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }  ]) 
+  const [persons, setPersons] = useState([]) 
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/db')
+      .then(resp => {
+        console.log('promise fulfilled', resp.data)
+        setPersons(resp.data.persons)
+      })
+  }, [])
+  console.log('render')
   const [searchStr, setSearchStr] = useState('')
 
   const handleSearchChange = (event) => {
