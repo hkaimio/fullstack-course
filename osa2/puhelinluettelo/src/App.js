bb853083756925c2dbe2b5d9483586f7bb24c384
Harri Kaimio <harri@kaimio.fi>
2023-05-12 20:09:53 +0300

    Ex 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const PhonebookEntry = ({person}) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map((person) => {
+          return (<PhonebookEntry key={person.name} person={person} />)
+        })}
+    </div>
+  )
+
+}
+
+export default App
