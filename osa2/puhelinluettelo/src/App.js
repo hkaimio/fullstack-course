563cd946e2ab8f3cebd92cb187cd6fb9582d2987
Harri Kaimio <harri@kaimio.fi>
2023-05-14 09:50:04 +0300

    Ex 2.17

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import phonebook from './services/phonebook'
+import './App.css'
 
 const SERVER_URL = 'http://localhost:3001'
 
-const PhonebookEntry = ({person}) => {
+const PhonebookEntry = ({person, handleDeleteBtnClicked}) => {
   return (
-    <p>{person.name}: {person.number}</p>
+    <p>{person.name}: {person.number} <button onClick={handleDeleteBtnClicked}>Delete</button></p>
   )
 }
 
@@ -31,7 +33,7 @@ const PersonForm = ({name, phone, handleNameChange, handlePhoneChange, handleAdd
   )  
 }
 
-const PersonList = ({persons, filterStr}) => {
+const PersonList = ({persons, filterStr, handleDeleteBtnClicked}) => {
   const filteredPersons = 
     filterStr === "" ? 
       persons : 
@@ -40,27 +42,51 @@ const PersonList = ({persons, filterStr}) => {
   return (
     <>
     {filteredPersons.map((person) => {
-      return (<PhonebookEntry key={person.id} person={person} />)
+      return (<PhonebookEntry 
+        key={person.id} 
+        person={person} 
+        handleDeleteBtnClicked={(event) => handleDeleteBtnClicked(event, person)}/>)
     })}
     </>
   )
 
 }
 
+const Notification = ({message}) => {
+  console.log('render message', message)
+  if (message === null) {
+    console.log('no message')
+    return null
+  } else {
+    console.log('show content!', message.text)
+    return (
+      <div id="notification-area" className={message.style}>{message.text}</div>
+    )
+  }
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get(`${SERVER_URL}/persons`)
-      .then(resp => {
-        console.log('promise fulfilled', resp.data)
-        setPersons(resp.data)
+    phonebook.getAll()
+      .then(allPersons => {
+        setPersons(allPersons)
       })
   }, [])
   console.log('render')
   const [searchStr, setSearchStr] = useState('')
+
+  const showMessage = (text, style) => {
+    console.log('showMessage', text, style)
+    setMessage({text, style})
+    setTimeout(() => {
+      console.log('remove message', text, style)
+      setMessage(null)
+    }, 3000)
+  }
 
   const handleSearchChange = (event) => {
     setSearchStr(event.target.value)
@@ -82,18 +108,31 @@ const App = () => {
   const addPerson = (person) => {
     console.log("Adding person ", person)
     // Check if person exists
-    if (persons.some( p => p.name.toLowerCase() === person.name.toLowerCase()))
-    {
-      alert(`${person.name} already exists in the phonebook`)
+    const matches = persons.filter(p => p.name.toLowerCase() === person.name.toLowerCase())
+    console.log('matches', matches)
+    if (matches.length > 0) {
+      const personId = matches[0].id
+      console.log('check if update', person)
+      if (window.confirm(`${person.name} already exists. Do you want to update the phone number?`)) {
+        phonebook.updatePerson(personId, person)
+          .then(updatedPerson => {
+            const newPersons = persons.map(p =>
+              p.id === personId ? updatedPerson : p)
+            setPersons(newPersons)
+            showMessage(`Modified ${updatedPerson.name}`, "info")
+          })
+      }
     }
-    else
-    {
-      axios.post(`${SERVER_URL}/persons`, person)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+    else {
+      console.log("no match, adding", person)
+      phonebook.create(person)
+        .then(newPerson => {
+          setPersons(persons.concat(newPerson))
+          showMessage(`Added ${newPerson.name}`, "info")
         })
     }
   }
+
 
   const addPersonBtnClicked = (event) => {
     event.preventDefault()
@@ -106,18 +145,36 @@ const App = () => {
     setNewPhone("")
   } 
 
-  const filteredPersons = 
-    searchStr === "" ? 
-      persons : 
-      persons.filter(person => person.name.toLowerCase().includes(searchStr.toLowerCase()))
+  const deletePerson = (person) => {
+    console.log("Deleting person", person)
+    phonebook.deletePerson(person.id)
+      .then ( () => {
+        console.log('deleted from database', person)
+        const newPersons = persons.filter(p => p.id !== person.id) 
+        setPersons(newPersons)
+        showMessage(`Deleted ${person.name}`, 'info')
+        console.log('deleted from local data', newPersons)
+      })
+  }
+
+  const personDeleteBtnClicked = (event, person) => {    
+    event.preventDefault()
+    console.log('delete btn handler', person)
+    deletePerson(person)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message}/>
       <Filter searchStr={searchStr} handleSearchStrChange={handleSearchChange}/>
       <PersonForm name={newName} phone={newPhone} handleNameChange={handleNewNameChange} handlePhoneChange={handleNewPhoneChange} handleAddPerson={addPersonBtnClicked}/>
       <h2>Numbers</h2>
-      <PersonList persons={persons} filterStr={searchStr}/>
+      <PersonList 
+        persons={persons} 
+        filterStr={searchStr}
+        handleDeleteBtnClicked={personDeleteBtnClicked}/>
     </div>
   )
 
